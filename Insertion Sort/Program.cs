03fc09045ac398a8f461a757497b2b8bfdc2f6b1
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-04-14 20:53:38 +0300

    Insertion Sort

--- Insertion Sort/Program.cs
+++ Insertion Sort/Program.cs
@@ -8,8 +8,45 @@ namespace Insertion_Sort
 {
     internal class Program
     {
+
+        static void InsertionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for ( int i = 1 ; i < n ; ++i )
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                // Move elements of arr[0..i-1], that are greater than key,
+                // to one position ahead of their current position
+                while ( j >= 0 && arr[j] > key )
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
         static void Main(string[] args)
         {
+
+            int[] arr = { 64 , 34 , 25 , 12 , 22 , 11 , 90 };
+
+            Console.WriteLine("Original array:");
+            foreach ( int i in arr )
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            InsertionSort(arr);
+
+            Console.WriteLine("\nSorted array:");
+            foreach ( int i in arr )
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
         }
     }
 }
